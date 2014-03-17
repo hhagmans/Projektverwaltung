78be0a1c8d013eba0277404c432500fb3f64db42
hhagmans <hendrik.hagmans@web.de>
2014-03-17 14:51:39 +0100

    First version oftemplates done, static templates

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,8 +5,27 @@ import play.mvc.Result;
 
 public class Application extends Controller {
 
-	public static Result index() {
-		return ok(views.html.index.render());
+	public static Result projectOverview() {
+		return ok(views.html.projectOverview.render());
 	}
 
+	public static Result userOverview() {
+		return ok(views.html.userOverview.render());
+	}
+
+	public static Result projectView() {
+		return ok(views.html.projectView.render());
+	}
+
+	public static Result projectEdit() {
+		return ok(views.html.projectEdit.render());
+	}
+
+	public static Result projectEditAddUser() {
+		return ok(views.html.projectEditAddUser.render());
+	}
+
+	public static Result userView() {
+		return ok(views.html.userView.render());
+	}
 }
